083af28c05bd9c056d7f8866fe89de06549d446b
minling <mzhao725@gmail.com>
2015-06-09 09:10:07 -0400

    jukebox

--- jukebox.rb
+++ jukebox.rb
@@ -11,3 +11,48 @@ songs = [
 ]
 
 # Your code here
+
+def run(songs)
+  puts "Please enter a command:"
+end
+
+def help
+  puts "help"
+  puts "list"
+  puts "play"
+  puts "exit"
+end
+
+def list(songs)
+  songs.each {|x| puts x}
+end
+
+def exit_jukebox
+  puts "Goodbye!"
+end
+
+def play(songs)
+  playsong = gets.chomp 
+  numbercheck = playsong.to_i
+
+  if numbercheck == 0 
+    songs.each_with_index do |value, index| 
+      if playsong.downcase == value.downcase 
+        puts songs[index]
+      end
+    end
+  else
+    if numbercheck > songs.length
+      puts "Invalid input, please try again"
+    else
+      puts songs[(numbercheck-1)]
+    end
+  end
+
+end
+
+
+
+
+
+
